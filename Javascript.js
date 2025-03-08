d8a2d16b33c2ed0d4baf285e4e3df162eb92d262
Mahaadyapayili <mahaadyapayili@gmail.com>
2025-03-08 20:08:44 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var a = 4;
+    document.getElementById("demo").innerHTML = a*a;
+}
+function myfunction1() {
+    var d = 6;
+    var g = 5;
+    document.getElementById("result").innerHTML = 1/2* d*g;
+}
